923cb70e04ec39ebd62050bd0ab5b9c3853bbe0d
MattSmith46 <MattSmith46@users.noreply.github.com>
2025-10-17 14:07:41 -0500

    Update to Terraform

--- scripts/create-resource-groups.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# This script is intentionally empty
-# Resource groups must be pre-created before Terraform deployment
-# This is a placeholder for documentation purposes
-
-echo "Resource groups must be created manually before deployment"
-echo "This deployment assumes all resource groups already exist"
-exit 0
